1bcf8108e03ac99a5f10c140b4e20989d10e449a
Julia de Lorimier <48865235+jldelorimier@users.noreply.github.com>
2024-02-09 02:38:15 -0500

    Add create_loan tests
    - Add two tests for the create_loan endpoint: test_create_loan happy path, and test_create_loan_user_does_not_exist

--- tests/test_main.py
+++ tests/test_main.py
@@ -59,3 +59,48 @@ def test_create_user_duplicate_email(client: TestClient):
     assert second_request_response.status_code == 400
     assert second_request_response.json().get('detail') == "Email already exists"
 
+# create_loan tests
+def test_create_loan(session: Session, client: TestClient): 
+    userB = User(name="test_userB@null.null", first_name="userB", last_name="lastnameB")
+    session.add(userB)
+    session.commit()
+
+    response = client.post(
+        "/loans/", json={
+            "amount": 1000,
+            "annual_interest_rate": 24,
+            "term_months": 36,
+            "user_id": 1
+        }
+    )
+
+    data = response.json()
+
+    # Assert loan creation response
+    assert response.status_code == 200
+    assert data["amount"] == "{:.6f}".format(Decimal(1000))
+    assert data["annual_interest_rate"] == "{:.5f}".format(Decimal(24))
+    assert data["term_months"] == 36
+    assert data ["id"] 
+
+    # Fetch UserLoanLink to verify creation
+    loan_id = data.get("id")
+    user_loan_link_query = select(UserLoanLink).filter_by(user_id=userB.id, loan_id=loan_id)
+    user_loan_link = session.exec(user_loan_link_query).first()
+
+    assert user_loan_link is not None
+    assert user_loan_link.user_id == userB.id
+    assert user_loan_link.loan_id == loan_id
+
+def test_create_loan_user_does_not_exist(client: TestClient):
+    nonexistent_user_id = 999
+    loan_data = {
+        "amount": 1000,
+        "annual_interest_rate": 5,
+        "term_months": 12,
+        "user_id": nonexistent_user_id
+    }
+    response = client.post("/loans/", json=loan_data)
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found"
